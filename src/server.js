2649311afdde7d0fafbfbae7fced68fc4cafaed5
Vanessa Cardoso <cardosovanessa@outlook.com>
2021-07-26 19:01:57 -0300

    porta pelo env

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const app = require('./app')
 const port = 3000
 
-app.listen(port, () => {
-  console.log('Servidor rodando na porta: ' + port)
+app.listen(process.env.PORT, () => {
+  console.log('Servidor rodando na porta: ' + process.env.PORT)
 })
+
+// app.listen(process.env.PORT, () => {
+//   console.log('Servidor rodando na porta: ' + port)
+// })
